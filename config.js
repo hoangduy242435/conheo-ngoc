d5f96218fb0a3b7c9754ddce16f38624b8d187c7
hoangduy242435 <85267583+hoangduy242435@users.noreply.github.com>
2021-06-03 10:31:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,15 +2,15 @@ const CONFIG = {
     introTitle: 'Ngọc iuu ơi Ngọc iu à!',
     introDesc: `Anh yêu bầu trời xanh, yêu cả cánh hoa hồng
     Nhưng không quên một thứ, đó chính là yêu em`,
-    btnIntro: 'Yeeuu <3',
+    btnIntro: 'Yêu <3',
     title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
     desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Thích anh và yêu anh rất nhìuuu <33',
     btnNo: 'Không, mình làm bạn thân đi :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều lắm đó công chúa của anh 😘😘',
+    reply: 'Không biết nữa em chỉ yêu a thôi <33333333',
+    mess: 'Anh biết mà 🥰. Anh cũng yêu em nhiều lắm đó công chúa của anh 😘😘',
     messDesc: 'Anh yêu em lắm nên em đừng buồn nữa nhaaa. Tối anh hát cho nghe <3333.',
     btnAccept: 'Okiiiii lun <3',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
